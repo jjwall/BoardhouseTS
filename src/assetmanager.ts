85bd4bee05516872f022307783acb38c5da05206
jjwall <jacob.wallace@utexas.edu>
2018-12-22 16:24:38 -0600

    Added promise for loading textures

--- src/assetmanager.ts
+++ src/assetmanager.ts
@@ -1,21 +1,20 @@
 import * as THREE from "three";
 
-export function loadTextures(urls: string[]) : UrlToTextureMap {
+export async function loadTextures(urls: string[]) : Promise<UrlToTextureMap> {
     let loader = new THREE.TextureLoader();
     let cachedTextures: UrlToTextureMap = {};
 
-    for(let i = 0; i < urls.length; i++) {
-        let texture = loader.load( urls[i], function(tex) {
-            if (i === (urls.length - 1)) {
-                return cachedTextures;
-            }
-            console.log( tex.image.width, tex.image.height );
-        });
+    return new Promise((resolve: (value: UrlToTextureMap) => void, reject) => {
+        for(let i = 0; i < urls.length; i++) {
+            let texture = loader.load( urls[i], function(tex) {
+                if (i === (urls.length - 1)) {
+                    resolve(cachedTextures);
+                }
+            });
 
-        cachedTextures[urls[i]] = texture;
-    }
-
-    return cachedTextures;
+            cachedTextures[urls[i]] = texture;
+        }
+    });
 }
 
 interface UrlToTextureMap {
