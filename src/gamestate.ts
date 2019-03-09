f67e007aeb69cdac16a9168e9d04a0f5e9ab4d1a
Jacob <jacob.wallace@utexas.edu>
2019-03-09 17:18:58 -0600

    Added ui camera and scene. Changed game camera coordinates.

--- src/gamestate.ts
+++ src/gamestate.ts
@@ -44,6 +44,8 @@ import { BaseState } from "./basestate";
 export class GameState extends BaseState {
     public gameScene: Scene;
     public gameCamera: Camera;
+    public uiScene: Scene;
+    public uiCamera: Camera;
     constructor(stateStack: BaseState[]) {
         super(stateStack);
         // Set up game scene.
@@ -51,8 +53,18 @@ export class GameState extends BaseState {
         this.gameScene.background = new Color("#FFFFFF");
 
         // Set up game camera.
-        this.gameCamera = new OrthographicCamera(1280 / - 2, 1280 / 2, 720 / 2, 720 / -2, -1000, 1000);
+        this.gameCamera = new OrthographicCamera(0, 1280, 720, 0, -1000, 1000);
         this.gameScene.add(this.gameCamera);
+
+        // Set up ui scene.
+        this.uiScene = new Scene();
+
+        // Set up ui camera.
+        this.uiCamera = new OrthographicCamera(0, 1280, 720, 0, -1000, 1000);
+
+        // Example ui element.
+        const ui = initializeSprite("./data/textures/cottage.png", this.uiScene, 1);
+        ui.position.set(200, 50, 0);
 
         // Register systems.
         this.registerSystem(controlSystem, "control");
@@ -66,7 +78,7 @@ export class GameState extends BaseState {
 
         // Set up player entity.
         let player = new Entity();
-        player.pos = initializePosition(100, -100, 5);
+        player.pos = initializePosition(150, 150, 5);
         player.sprite = initializeSprite("./data/textures/msknight.png", this.gameScene, 4);
         player.control = initializeControls();
         player.vel = initializeVelocity(1);
@@ -74,16 +86,16 @@ export class GameState extends BaseState {
         player.anim = initializeAnimation(SequenceTypes.walk, playerAnim);
         player.hurtBox = initializeHurtBox(player.sprite, HurtBoxTypes.test, 50, 50, -300, -100);
         player.timer = initializeTimer(250, () => { 
-            this.removeEntity(player);
+            // this.removeEntity(player);
             // Remove player sprite from scene.
-            this.gameScene.remove(player.sprite);
+            // this.gameScene.remove(player.sprite);
         });
         setHurtBoxGraphic(player.sprite, player.hurtBox);
         this.registerEntity(player);
 
         // Set up enemy entity.
         let enemy = new Entity();
-        enemy.pos = initializePosition(-300, -100, 4);
+        enemy.pos = initializePosition(300, 100, 4);
         enemy.sprite = initializeSprite("./data/textures/cottage.png", this.gameScene, 4);
         enemy.hitBox = initializeHitBox(enemy.sprite, [HurtBoxTypes.test], 50, 50, 100, 200);
         setHitBoxGraphic(enemy.sprite, enemy.hitBox);
@@ -101,6 +113,9 @@ export class GameState extends BaseState {
     public render(renderer: WebGLRenderer) : void {
         renderer.clear();
         renderer.render(this.gameScene, this.gameCamera);
+        renderer.clearDepth();
+        renderer.render(this.uiScene, this.uiCamera);
+
         // check if children needs to be reconciled, then do so
         // BoardhouseUI.ReconcilePixiDom(this.rootWidget, stage);
     }
