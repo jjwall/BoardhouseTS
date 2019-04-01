80618b128af9a9b9592bcc4b89313e2cfe9df4e6
jjwall <jacob.wallace@utexas.edu>
2019-03-31 20:37:47 -0500

    Babel now transpiles jsx elements to js objects

--- src/gameui.tsx
+++ src/gameui.tsx
@@ -1,37 +1,49 @@
 import { JSX } from "./ui/interfaces";
+// /** @jsx createElement */
 import { createElement } from "./ui/createelement";
 
 export function createGameUi() : JSX.Element {
-    return createElement(
-        "panel",
-        {
-            height: 70,
-            width: 300,
-            color: "#228B22",
-            onClick: () => console.log("event!!!"),
-            img: "./data/textures/cottage.png",
-            top: 50,
-            left: 500,
-        },
-        createElement("panel",
-            {
-                height: 50,
-                width: 50,
-                color: "#00FFFF",
-                img: "./data/textures/cottage.png",
-                // top: 0,
-                // left: 0,
-                z_index: 1,
-            },
-            createElement(
-                    "label",
-                    {
-                        nodeValue: "blah",
-                        top: 10,
-                        color: "#0000FF",
-                        // font_size: 30,
-                    }
-            )
-        )
+    return (
+        <panel 
+            height="70" width="300" color="#228B22"
+            top="50" left="500">
+            <panel 
+                height="50" width="50" color="#00FFFF"
+                img="./data/textures/cottage.png">
+                {/* <label>blah</label> */}
+            </panel>
+        </panel>
     );
+    // createElement(
+    //     "panel",
+    //     {
+    //         height: 70,
+    //         width: 300,
+    //         color: "#228B22",
+    //         onClick: () => console.log("event!!!"),
+    //         img: "./data/textures/cottage.png",
+    //         top: 50,
+    //         left: 500,
+    //     },
+    //     createElement("panel",
+    //         {
+    //             height: 50,
+    //             width: 50,
+    //             color: "#00FFFF",
+    //             img: "./data/textures/cottage.png",
+    //             // top: 0,
+    //             // left: 0,
+    //             z_index: 1,
+    //         },
+    //         createElement(
+    //                 "label",
+    //                 {
+    //                     nodeValue: "blah",
+    //                     top: 10,
+    //                     color: "#0000FF",
+    //                     // font_size: 30,
+    //                 }
+    //         )
+    //     )
+    // );
 }
